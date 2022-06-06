e1bafe82539d024a6dd58cebd7c589904c6dcc10
cardoso <cardoso.gabriel@grad.ufsc.br>
2022-06-06 15:00:54 -0300

    Limpa o script do main

--- src/main/java/RNG.java
+++ src/main/java/RNG.java
@@ -12,40 +12,29 @@ public class RNG {
     public static void main(String[] args) {
         List<Integer> sizes = Arrays.asList( 128, 168, 224, 256, 512, 1024, 2048, 4096);
 
-//        measurePrimeGenerationTime(new Xorshift(), sizes);
         BigInteger64BitRngEngine engine_64 = new BigInteger64BitRngEngine();
         BigInteger32BitRngEngine engine_32 = new BigInteger32BitRngEngine();
 
         engine_64.start(12, new Xorshift());
         engine_32.start(12, new MersenneTwister());
 
-//        measurePrimeGenerationTime(engine_64, sizes);
+        measureGenerationTime(engine_32, sizes);
+        measureGenerationTime(engine_32, sizes);
+
         measurePrimeGenerationTime(engine_32, sizes);
+        measurePrimeGenerationTime(engine_64, sizes);
 
-
-//        System.out.println(MillerRabin.checkPrimalty(BigInteger.valueOf(59), 5));
-//        measureGenerationTime(new Xorshift());
-//        measureGenerationTime(new BlumBlumShub());
-        int mt19937_seed = (int)System.currentTimeMillis();
-        System.out.println("Seed: " + mt19937_seed);
-
-        MersenneTwister mt19937 = new MersenneTwister(mt19937_seed);
-        List<Integer> sample = new ArrayList<Integer>();
-        for (int i=0; i<10; i++) {
-            sample.add(mt19937.nextInt());
-        }
-        System.out.println(crackMT19937Seed(sample,3600000, mt19937_seed + 3600000));
+        crackMT19937Seed();
     }
 
     public static void measureGenerationTime(BigIntegerRNG rng, List<Integer> sizes) {
         for (Integer size : sizes) {
             rng.setSeed(new SecureRandom().nextLong());
             long start = System.currentTimeMillis();
-            rng.nextValue(size);
-            System.out.printf("Size of %d takes %d milliseconds%n", size, (System.currentTimeMillis() - start));
+            for (int i = 0; i < 1000000; i++) rng.nextValue(size);
+            System.out.printf("PRIME Size of %d takes %d milliseconds%n", size, (System.currentTimeMillis() - start));
         }
     }
-
     public static void measurePrimeGenerationTime(BigIntegerRNG rng, List<Integer> sizes) {
         for (Integer size : sizes) {
             rng.setSeed(new SecureRandom().nextLong());
@@ -57,8 +46,18 @@ public class RNG {
             System.out.printf("PRIME Size of %d takes %d milliseconds%n", size, (System.currentTimeMillis() - start));
         }
     }
+    public static void crackMT19937Seed() {
+        int mt19937_seed = (int)System.currentTimeMillis();
+        int time_to_rollback = 3600000;
+        int time_start = mt19937_seed + 3600000;
+        System.out.println("Seed: " + mt19937_seed);
 
-    public static int crackMT19937Seed(List<Integer> sample, int time_to_rollback, int time_start) {
+        MersenneTwister mt19937 = new MersenneTwister(mt19937_seed);
+        List<Integer> sample = new ArrayList<Integer>();
+        for (int i=0; i<10; i++) {
+            sample.add(mt19937.nextInt());
+        }
+
         for (int i = time_start; i >= time_start-time_to_rollback; i--) {
             MersenneTwister rng = new MersenneTwister(i);
             Iterator<Integer> it = sample.iterator();
@@ -66,9 +65,9 @@ public class RNG {
                 if(it.next() != rng.nextInt()) {
                     break;
                 }
-                return i;
+                System.out.println(mt19937_seed);
+                break;
             }
         }
-        return 0;
     }
 }
